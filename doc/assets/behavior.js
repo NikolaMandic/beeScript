debaf516f9c441ed3d941eca04de1dc180912be3
NikolaMandic <mn080202@gmail.com>
2013-10-24 05:33:03 +0200

    s

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -9,6 +9,22 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
       "type": "file",
       "data": {
         "language": {
+          "nameMatchers": [".md"],
+          "commentsOnly": true,
+          "name": "Markdown"
+        },
+        "sourcePath": "/home/uname/beeScript/README.md",
+        "projectPath": "README.md",
+        "targetPath": "index",
+        "pageTitle": "index",
+        "title": "index"
+      },
+      "depth": 1,
+      "outline": []
+    }, {
+      "type": "file",
+      "data": {
+        "language": {
           "nameMatchers": [".coffee", "Cakefile"],
           "pygmentsLexer": "coffee-script",
           "multiLineComment": ["###*", " *", " ###", "###", "#", "###"],
